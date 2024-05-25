4bfb62a347324dd8bc5f3d27cc6d0ed293938f3e
Nariman Biiazbekov <narbiiaz@gmail.com>
2024-05-25 11:57:21 +0600

    Добавлена модель User. Подключены библиотеки. Настроена база данных. Выполнены миграции

--- Forum/Program.cs
+++ Forum/Program.cs
@@ -1,9 +1,40 @@
+using Forum.Models;
+using Forum.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ForumContext>(opts => opts.UseNpgsql(connection))
+    .AddIdentity<User, IdentityRole<int>>(opts =>
+    {
+        opts.Password.RequireNonAlphanumeric = false;
+        opts.Password.RequireLowercase = false;
+        opts.Password.RequireUppercase = false;
+        opts.Password.RequireDigit = false;
+        opts.Password.RequiredLength = 5;
+    })
+    .AddEntityFrameworkStores<ForumContext>();
+
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole<int>>>();
+    await AdminInitializer.SeedAdminUser(roleManager, userManager);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error has occured while seeding the database");
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,7 +48,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
